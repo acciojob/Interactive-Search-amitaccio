bbead68cdc7723fe9a12043c1bc4fcf62e6a466a
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-21 19:32:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-//your JS code here. If required.
-//your JS code here. If required.
-const button = document.querySelector(".btn");
-button.addEventListener("click",()=> {
-	button.classList.add(".active");
+// Get references to the HTML elements
+const searchContainer = document.querySelector(".search");
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+
+// Toggle the "active" class on the search container
+searchButton.addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    if (searchContainer.classList.contains("active")) {
+        // Focus on the input element after making it active
+        searchInput.focus();
+    }
 });
