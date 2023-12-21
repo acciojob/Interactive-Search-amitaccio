2408f4d3ef484953d43148022e8484dbddf1a0cc
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-21 19:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const button = document.querySelector(".btn");
+button.addEventListener("click",()=> {
+	button.classList.add(".active");
+});
